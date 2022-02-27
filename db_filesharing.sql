b07fefaba2cf0416a455c4bc64bb6e5c3d833697
nurilhdh <nurilhd29@gmail.com>
2022-02-27 23:49:31 +0700

    file

---
+++ db_filesharing.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 26, 2022 at 05:51 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_filesharing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uploaded_files`
+--
+
+CREATE TABLE `uploaded_files` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `new_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `uploaded_files`
+--
+
+INSERT INTO `uploaded_files` (`id`, `name`, `new_name`) VALUES
+(1, 'text-animation.zip', '2711201606481769text-animation.zip'),
+(2, 'TL.png', '2711201606481879TL.png'),
+(3, 'TL.png', '2711201606482588TL.png'),
+(4, 'TL.png', '2711201606482604TL.png'),
+(5, 'TL.png', '2711201606482636TL.png'),
+(6, 'text-animation.zip', '2711201606483139text-animation.zip'),
+(7, '01. Modul pengenalan kabel TIA.pdf', '240222164568698001. Modul pengenalan kabel TIA.pdf'),
+(8, '1.  Sistem Komunikasi Seluler.pdf', '25022216457694991.  Sistem Komunikasi Seluler.pdf'),
+(9, '03_Nuril Hidayah.pdf', '250222164580377403_Nuril Hidayah.pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, 'Nuril Hidayah', 'nurilhd29@gmail.com', '0139a3c5cf42394be982e766c93f5ed0'),
+(2, 'user', 'user@gmail.com', 'd397e36f9ebadc4bebcd21874cbed561');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `uploaded_files`
+--
+ALTER TABLE `uploaded_files`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `uploaded_files`
+--
+ALTER TABLE `uploaded_files`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
